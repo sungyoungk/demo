34529c3399ccd1fca5f4ba7391673273a268f96f
updown-90 <sangha.kim@kurlycorp.com>
2023-11-04 18:30:38 +0900

    fix: 방명록 작성시 validation 체크 추가

--- src/main/java/com/example/demo/config/MvcConfiguration.java
+++ src/main/java/com/example/demo/config/MvcConfiguration.java
@@ -12,8 +12,8 @@ public class MvcConfiguration implements WebMvcConfigurer {
 	@Override 
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		/* '/js/**'로 호출하는 자원은 '/static/js/' 폴더 아래에서 찾는다. */ 
-        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/resources/assets/").setCachePeriod(60 * 60 * 24 * 365);
-        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/resources/js/").setCachePeriod(60 * 60 * 24 * 365);
+        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/").setCachePeriod(60 * 60 * 24 * 365);
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/js/").setCachePeriod(60 * 60 * 24 * 365);
 		/* '/css/**'로 호출하는 자원은 '/static/css/' 폴더 아래에서 찾는다. */
         registry.addResourceHandler("/css/**").addResourceLocations("classpath:/css/").setCachePeriod(60 * 60 * 24 * 365);
 		/* '/img/**'로 호출하는 자원은 '/static/img/' 폴더 아래에서 찾는다. */ 
